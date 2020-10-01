2e0a57562ca31e00b6e56e7e35eb4cd6bb34db7d
mcjay18 <40698227+mcjay18@users.noreply.github.com>
2020-10-01 11:30:49 -0500

    adding base files

---
+++ Scripts/CM_Report_V01.py
@@ -0,0 +1,373 @@
+#!/bin/usr/env python3
+
+import yaml 
+import json
+import csv
+import argparse
+
+
+'''
+Versioning: 
+    1.0 Inital script 
+    1.1 added cmd options for inv and report files 
+    
+'''
+############################################################################################
+#Script to generate CSV report from the output of Ansible CableDiag playbook
+#
+#
+############################################################################################
+__author__ = "Jay McNealy"
+__copyright__ = "None"
+__credits__ = ["Jay McNealy"]
+__license__ = "GPL"
+__version__ = "1.1"
+__maintainer__ = "Jay McNealy"
+__email__ = "justin.mcnealy@hpe.com"
+__status__ = "Testing"
+############################################################################################
+
+
+# pull raw data from ansible output
+def getraw(rawdata):
+    # print (rawdata['stdout_lines'])
+
+    return((rawdata['stdout_lines']))
+
+# create Dict from raw cable diagnostic data
+def cbldiag_parse(cbldata):
+    portdict = {}
+    #print (type(data))
+    for q in cbldata:
+        
+        if '1-2' in q: 
+            qsp = q.split()
+            port = qsp[0]
+            porttup = qsp[1],qsp[2],qsp[3]
+            portdict[port]= [(porttup)]
+            #print (qsp)
+        elif '3-6' in q or '4-5' in q or '7-8' in q:
+            qsp = q.split()
+            porttup = qsp[0],qsp[1],qsp[2]
+            portdict[port].append(porttup)
+    return (portdict)
+
+# create Dict from raw mac address data
+def macaddress_parse(macdata):
+    #print (macdata)
+    with open('mac_oui.json','r') as tempoui:
+        ouidict = json.loads(tempoui.read())
+    macdict = {}
+    for w in macdata:
+        #print (w)
+        if 'Status' in w or 'MAC Address' in w or '-----' in w or w == "" or 'Attempting' in w:
+            continue
+        else:
+            wspl = w.split()
+            #print (wspl)
+            mac = (wspl[0])
+            macslt = mac.split('-')
+            pre_oui = macslt[0]
+            try:
+                #print (ouidict[pre_oui])
+                oui = ouidict[pre_oui]
+            except:
+                oui = 'Not Found'
+            if 'Trk' not in wspl[1]:
+                macdict[wspl[1]] =  (wspl[0],wspl[2],oui)
+    #print (json.dumps(macdict,indent=2))
+    return (macdict)
+
+# create Dict from raw interface data (idf)
+def interface_parse(intdata):
+    #print (intdata)
+    sw_Int_Dict = {}
+    for e in intdata:
+        #print (e)
+        portdict = {}
+        if 'Status' in e or 'Port' in e or '-----' in e or 'Flow' in e or e == "" or 'Attempting' in e or 'show'in e:
+            continue
+        else:
+        
+            espl = e.split()
+            portdict['Tot_Bytes']= espl[1]
+            portdict['Tot_Frames']= espl[2]
+            portdict['Err_RX']= espl[3]
+            portdict['Drop_TX']= espl[4]
+        sw_Int_Dict[espl[0]]=portdict
+    return (sw_Int_Dict)
+
+# create Dict from raw Interface data (MDF)
+def mdf_interface_parse(intdata):
+    #print (intdata)
+    sw_Int_Dict = {}
+    for e in intdata:
+        #print (e)
+        portdict = {}
+        if 'Status' in e or 'Port' in e or '-----' in e or 'Flow' in e or e == "" or 'Attempting' in e or 'show' in e:
+            continue
+        else:
+            espl = e.split()
+            print (espl)
+            portdict['Tot_Bytes']= espl[1]
+            portdict['Tot_Frames']= espl[2]
+            portdict['Err_RX']= espl[3]
+            portdict['Drop_TX']= espl[4]
+        sw_Int_Dict[espl[0]]=portdict
+    return (sw_Int_Dict)
+
+# create Dict from raw interface brief data 
+def interfacebrief_parse(intbriefdata):
+    #print (intdata)
+    sw_brief_Int_Dict = {}
+    for u in intbriefdata:
+        u = u.replace('|','')
+        #print (u)
+        u = u.replace('.','')
+        portBrieDict = {}
+        #print (u)
+        if 'Status' in u or 'Port' in u or '-----' in u or 'Flow' in u or u == "" or 'Attempting' in u:
+            continue
+        else:
+            uspl = u.split()
+            #print (len(uspl))
+            if (len(uspl)) == 9:
+                portBrieDict['Type']= uspl[1]
+                portBrieDict['Enabled']= uspl[3]
+                portBrieDict['Status']= uspl[4]
+                portBrieDict['Mode']= uspl[5]  
+            elif (len(uspl)) == 6:
+                portBrieDict['Type']= "MIA"
+                portBrieDict['Enabled']= uspl[2]
+                portBrieDict['Status']= uspl[3]
+                portBrieDict['Mode']= uspl[4] 
+            else:
+                print (len(uspl))
+            #print (uspl)
+        sw_brief_Int_Dict[uspl[0]]= portBrieDict
+    #print (sw_brief_Int_Dict)
+    return (sw_brief_Int_Dict)
+
+# Create Dict from raw poe data
+def poe_parse(poedata):
+    poedict = {}
+    for poeline in poedata:
+        if '----' in poeline or 'Port' in poeline or 'Member' in poeline or 'Remaining' in poeline or poeline == "" or 'Refer' in poeline or 'Status' in poeline:
+            continue
+        else:
+            poelinespt = poeline.split()
+            #print (poelinespt)
+            # key is port number. tuple is ( POE enabled, Delivering )
+            poedict[poelinespt[0]]= (poelinespt[1],poelinespt[10])
+    #print (json.dumps(poedict,indent=2))
+    return (poedict)
+
+# combine all dicte into 1 with interface as index 
+def combine_dict(cable,macadd,interface,brief,poe):
+    for r in interface:
+        #print (r)
+        try:
+            #print ('match Mac')
+            interface[r]['macaddr'] = (macadd[r])
+        except:
+            interface[r]['macaddr'] = 'N/A'
+        # print (macadd[r])
+        try:
+           #print('match cable')
+           interface[r]['cablediag'] = (cable[r])
+           #print ('Mac Fount for {}'.format (r))
+        except:
+            interface[r]['cablediag'] = 'N/A'
+        try:
+            interface[r]['intBrief'] = brief[r]
+        except:
+            interface[r]['intBrief'] = "Missing"
+        try:
+            interface[r]['poe'] = poe[r]
+        except:
+            interface[r]['poe'] = "N/A"
+
+    return(interface)
+
+# pull info from combine to create csv dict then write line to file 
+def csvstarter(csvdata,rep_file):
+        #CSV formating and header
+        csvfile = open(rep_file, 'w', newline='')
+        csv_columns = ['switch','port','Tot Bytes','Tot Frames','Err RX',\
+                        'TX Drop','Mac','Mac vendor','Mac Vlan','Enabled','Status','Mode','poe','D_1-2 stat','D_1-2 length',\
+                        'D_3-6 stat','D_3-6 length','D_4-5 stat','D_4-5 length','D_7-8 stat','D_7-8 length']
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for a in csvdata:
+            csv_dict ={}
+            csv_dict['switch'] = a
+            #print (a)
+            for s in csvdata[a]:
+                portdata = csvdata[a][s]
+                #print (portdata)
+                csv_dict['port'] = "- " + s + " -"
+                csv_dict['Tot Bytes'] = portdata['Tot_Bytes']
+                csv_dict['Tot Frames'] = portdata['Tot_Frames']
+                csv_dict['Err RX'] = portdata['Err_RX']
+                csv_dict['TX Drop'] = portdata['Drop_TX']
+                if portdata['macaddr'] == 'N/A':
+                    csv_dict['Mac'] = 'N/A'
+                    csv_dict['Mac Vlan'] = 'N/A'
+                    csv_dict['Mac vendor'] = 'N/A'
+                else: 
+                    csv_dict['Mac'] = portdata['macaddr'][0]
+                    csv_dict['Mac Vlan'] = portdata['macaddr'][1]
+                    csv_dict['Mac vendor'] = portdata['macaddr'][2]
+                csv_dict['Enabled'] = portdata['intBrief']['Enabled']
+                csv_dict['Status'] = portdata['intBrief']['Status']
+                csv_dict['Mode'] = portdata['intBrief']['Mode']
+                csv_dict['poe'] = portdata['poe'][1]
+                #print (portdata['intBrief']['Status'])
+                if portdata['cablediag'] == 'N/A':
+                    csv_dict['D_1-2 stat'] = 'N/A'
+                    csv_dict['D_1-2 length'] = 'N/A'
+                    csv_dict['D_3-6 stat'] = 'N/A'
+                    csv_dict['D_3-6 length'] = 'N/A'
+                    csv_dict['D_4-5 stat'] = 'N/A'
+                    csv_dict['D_4-5 length'] = 'N/A'
+                    csv_dict['D_7-8 stat'] = 'N/A'
+                    csv_dict['D_7-8 length'] = 'N/A'
+                else:
+                    #print (portdata['cablediag'][0][1])
+                    csv_dict['D_1-2 stat'] = portdata['cablediag'][0][1]
+                    csv_dict['D_1-2 length'] = portdata['cablediag'][0][2]
+                    csv_dict['D_3-6 stat'] = portdata['cablediag'][1][1]
+                    csv_dict['D_3-6 length'] = portdata['cablediag'][1][2]
+                    csv_dict['D_4-5 stat'] = portdata['cablediag'][2][1]
+                    csv_dict['D_4-5 length'] = portdata['cablediag'][2][2]
+                    csv_dict['D_7-8 stat'] = portdata['cablediag'][3][1]
+                    csv_dict['D_7-8 length'] = portdata['cablediag'][3][2]
+                writer.writerow(csv_dict)
+        return()
+
+
+
+
+results_filder = 'Results/1777-r3/'
+
+# Arg Parse Main start
+# Parse Command Line Arguments
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+        description='Basic Discription: This script will use Aruba Central APIs to generate yaml invintory with hostname, ports, and mgmt ip for THD stores')
+#group set up for further development
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-i","--invintory", help="Input file", type=str)
+parser.add_argument("-f","--file", help="report filename if differant then inv file (csv will be appended)", type=str)
+parser.add_argument("-v", help="Verbose Output", action="store_true")
+args = parser.parse_args()
+
+# gather files. If no output file is selected, will write with invintorey filename (as csv and raw.json)
+inv_file = args.invintory
+if args.file:
+    rep_file = args.file + '.csv'
+    raw_file = args.file + 'raw.json'
+else:
+    rep_file = inv_file.replace('.yml','.csv')
+    raw_file = inv_file.replace('.yml','raw.json')
+    
+try:
+    with open(inv_file,'r') as file:
+        devlst = yaml.load(file,Loader=yaml.FullLoader)
+except:
+    print ("[*] Error Could not find invintory file")
+    exit()
+
+
+
+inv_dict = devlst['all']
+idf_lst = []
+mdf_lst = []
+final_dict = {}
+for idfs in (inv_dict['children']['idf']['hosts']):
+    idf_lst.append(idfs)
+for mdfs in (inv_dict['children']['mdf']['hosts']):
+    mdf_lst.append(mdfs)
+for idf in idf_lst:
+    try:
+        filename = results_filder + idf + '.cable'
+        with open(filename, 'r') as idffile:
+            cableraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + idf + '.interface'
+        with open(filename, 'r') as idffile:
+            interfaceraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + idf + '.interfacebrie'
+        with open(filename, 'r') as idffile:
+            intbriraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + idf + '.macaddress'
+        with open(filename, 'r') as idffile:
+            macraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + idf + '.poe'
+        with open(filename, 'r') as idffile:
+            poeraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    cabldict = cbldiag_parse(cableraw)
+    macadd_dict = macaddress_parse(macraw)
+    int_dict = interface_parse(interfaceraw)
+    int_brief_dict = interfacebrief_parse(intbriraw)
+    poe_dict = poe_parse(poeraw)
+    final_dict[idf] = combine_dict(cabldict,macadd_dict,int_dict,int_brief_dict,poe_dict)
+
+for mdf in mdf_lst:
+    mac_table = None
+    int_brief = None
+    int_stat = None
+    Cbldiag = None
+    try:
+        filename = results_filder + mdf + '.cable'
+        with open(filename, 'r') as idffile:
+            cableraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + mdf + '.interface'
+        with open(filename, 'r') as idffile:
+            interfaceraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + mdf + '.interfacebrie'
+        with open(filename, 'r') as idffile:
+            intbriraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + mdf + '.macaddress'
+        with open(filename, 'r') as idffile:
+            macraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    try:
+        filename = results_filder + mdf + '.poe'
+        with open(filename, 'r') as idffile:
+            poeraw = getraw(json.loads(idffile.read()))
+    except:
+        print ("filenotfound")
+    cabldict = cbldiag_parse(cableraw)
+    macadd_dict = macaddress_parse(macraw)
+    int_dict = interface_parse(interfaceraw)
+    int_brief_dict = interfacebrief_parse(intbriraw)
+    poe_dict = poe_parse(poeraw)
+    final_dict[mdf] = combine_dict(cabldict,macadd_dict,int_dict,int_brief_dict,poe_dict)
+with open(raw_file,'w') as y:
+    y.write(json.dumps(final_dict,indent=2))
+
+csvstarter(final_dict,rep_file)
---
+++ Scripts/InvMake_v01.py
@@ -0,0 +1,371 @@
+import requests
+import json
+import configparser
+import sys
+import ast
+import argparse
+import getpass
+import logging
+import time
+#from datetime import datetime
+import datetime
+from subprocess import check_output
+from collections import defaultdict
+import yaml
+
+
+'''
+TODO notes:
+
+Adjust logging 
+Add store validation 
+'''
+
+__author__ = "Jay McNealy"
+__copyright__ = "None"
+__credits__ = ["Jay McNealy"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Jay McNealy"
+__email__ = "justin.mcnealy@hpe.com"
+__status__ = "Testing"
+
+# Logging
+def init_logging():
+
+    'Initialize Logging Globally'
+
+    # Specify our log format for handlers
+    log_format = logging.Formatter('%(asctime)s %(name)s:%(levelname)s: %(message)s')
+
+    # Get the root_logger to attach log handlers to
+    root_logger = logging.getLogger()
+
+    # Set root logger to debug level always
+    # Control log levels at log handler level
+    root_logger.setLevel(logging.DEBUG)
+
+    # Console Handler (always use log_level)
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level)
+    ch.setFormatter(log_format)
+    root_logger.addHandler(ch)
+
+
+    # Logfile Handler
+    fh = logging.FileHandler(config['main']['log_file'])
+
+    # Always log at INFO or below
+    if log_level < logging.INFO:
+        fh.setLevel(log_level)
+    else:
+        fh.setLevel(logging.INFO)
+
+    # Attach logfile handler
+    fh.setFormatter(log_format)
+    root_logger.addHandler(fh)
+
+# Central API to get list of switches for group
+def get_switches(host,group,token):
+    url = host +"/monitoring/v1/switches?group="+group+"&show_resource_details=true"
+    #print (url)
+    payload = {}
+    headers = {
+    'Authorization': 'bearer %s'%token
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    if response.status_code == 200:
+        logger.info(response.text.encode('utf8'))
+        switches_json = json.loads(response.text.encode('utf8'))
+        print('[+] Get Switches API returned with status code 200')
+        return(switches_json["switches"])
+    else:
+        logger.warning ("[-] Error: Get_Switches API call did not return with response code 200")
+        logger.warning (response.text)
+        logger.warning ("ERROR 14")
+        print ("[-] Error: Get_Switches API call did not return with response code 200")
+        sys.exit()
+
+# Central api to get group from search string
+def get_group(host,srch,token):
+    url = host +"/configuration/v1/groups?limit=20&offset=0&q="+srch
+    #print (token)
+    payload = {}
+    headers = {
+    'Authorization': 'bearer %s'%token
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    #return(True)
+    logger.info(response.text.encode('utf8'))
+    if response.status_code == 200:
+        logger.info(response.text.encode('utf8'))
+        resp_json = json.loads(response.text.encode('utf8'))
+        if len(resp_json['data']) > 1 or len(resp_json['data']) == 0:
+            print('Search returned {} matches. Please try again with a more spicific search paramiter.'.format(len(resp_json['data'])))
+            sys.exit()
+            #print(len(resp_json['data']))
+        print('[+] Get group API returned with status code 200')
+        return(resp_json['data'][0])
+    else:
+        logger.warning ("[-] Error: Get_Group API call did not reutn with response code 200")
+        logger.warning (response.text)
+        logger.warning ("ERROR 15")
+        print ("[-] Error: Get_Group API call did not reutn with response code 200")
+        sys.exit()
+
+# Central API to get vlan 4093 IP address
+def get_4093ip(host,swinfo,token):
+    if swinfo['stackstat'] is False:
+        url = host + "/monitoring/v1/switches/"+swinfo['stack_id']+"/vlan?id=4093"
+    else:
+        url = host +"/monitoring/v1/switch_stacks/"+swinfo['stack_id']+"/vlan?id=4093"
+    #print (url)
+    payload = {}
+    headers = {
+    'Authorization': 'bearer %s'%token
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    #return(True)
+    response_json = json.loads(response.text)
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        logger.info(response.text)
+        #print (response_json.keys())
+        vlans = response_json['vlans']
+        print('[+] Get vlan 4093 ip address API returned with status code 200')
+        return(vlans[0]['ipaddress'])
+    else:
+        logger.warning ("[-] Error: Get_4093ip API call did not reutn with response code 200")
+        logger.warning (response.text)
+        logger.warning ("ERROR 16")
+        print ("[-] Error: Get_4093ip API call did not reutn with response code 200")
+        sys.exit()
+
+# Central API to "RJ-45" ports form MDF 
+def get_mdfports(host,data,token):
+    ports = []
+    for sw in data:
+        #print(sw[2])
+        url = host + '/monitoring/v1/switches/'+ sw[2] +'/ports'
+        #print (url)
+        payload = {}
+        headers = {
+        'Authorization': 'bearer %s'%token
+        }
+
+        response = requests.request("GET", url, headers=headers, data = payload)
+        if response.status_code == 200:
+            logger.info(response.text)
+            switches_json = json.loads(response.text)
+            returnedports = mdfportparser(switches_json)
+
+            ports += returnedports
+
+        else:
+            logger.warning ("[-] Error: get_MDFports API call did not reutn with response code 200")
+            logger.warning (response.text)
+            logger.warning ("ERROR 17")
+            print ("[-] Error: get_MDFports API call did not reutn with response code 200")
+            sys.exit()
+    portsstr = ",".join(ports)
+    print('[+] Get mdf ports returned with status code 200')
+    return(portsstr)
+
+# function to process get_mdfports data
+def mdfportparser(mdfdata):
+    singleports = []
+    #print (data['ports'])
+    print('[+] Parsing data from MDF port API call')
+    for port in mdfdata['ports']:
+        if port['phy_type'] == 'RJ45' and 'oobm' not in port['port_number'] and "A" not in port['port_number'] and "B" not in port['port_number']:
+            singleports.append(port['port_number'])
+    singlestr= ",".join(singleports)
+    #singleste = "\' " + singlestr + " \'"
+    #print (singlestr + '\n\n\n\n')
+    return (singleports)
+
+# Function to convert IDF models to port lists
+def portconverter(idfdata):
+    portlist = []
+    portstr = ""
+    #print ((data))
+    print('[+] Get IDF ports from model number')
+    for sw in idfdata:
+        #print (sw[0])
+        if 'J9727A' in sw[0]:
+            swports = "{}/1-{}/23".format (sw[1],sw[1])
+            portlist.append(swports)
+        elif 'JL320A' in sw[0]:
+            swports = "{}/1-{}/23".format (sw[1],sw[1])
+            portlist.append(swports)
+        elif 'JL258A' in sw[0]:
+            swports = "{}/1-{}/8".format (sw[1],sw[1])
+            portlist.append(swports)
+    portstr = ",".join(portlist)
+    #portstr = "\" " + portstr + " \""
+    #print (portstr)
+    ###### Changed portstr to string need to 
+    return (portstr)
+
+# Ag function used to build inbvintory Dictionary from get_switches API call
+def swData(switches):
+    print('[+] Updating Switch Dictionary')
+    switchnames =[]
+    stackinfo = {}
+    storeinfo = {}
+    for switch in switches:
+        if switch["name"] not in switchnames and switch["status"] != 'Down':
+            switchnames.append(switch["name"])
+        elif switch["name"] not in switchnames and switch["status"] == 'Down':
+            print ('[-] ERROR: Switch not up.{}'.format(switch["name"]))
+        else:
+            continue
+    for swich in switchnames:
+        swichinfo = {}
+        swichlist = []
+        for switch in switches:
+            if switch['name'] == swich:
+                try:
+                    swtup = (switch["model"],switch["stack_member_id"],switch['serial'])
+                    swichlist.append(swtup)
+                except:
+                    swtup = (switch["model"],1,switch['serial'])
+                    swichlist.append(swtup)
+
+                #Remove once MDF address is converted to ATT IP address
+                if switch["ip_address"] != "":
+                    swichinfo['ip'] = switch["ip_address"]
+                try:
+                    swichinfo['stack_id'] = switch['stack_id']
+                    swichinfo['stackstat'] = True
+                except:
+                    swichinfo['stack_id'] = switch['serial']
+                    swichinfo['stackstat'] = False
+        swichinfo['switches'] = swichlist
+        storeinfo[swich]=swichinfo
+    
+    return(storeinfo)
+
+# function to write Yaml file from inv Dict
+def ymlmaker(mdf,idf):
+    iterat1 = {}
+    idfs = {}
+    mdfs = {}
+    devlist = []
+    base = {}
+    completedict = {}
+    idffullsw = {}
+    mdffullsw = {}
+    for sw in idf:
+        intsw = {}
+        intsw['ports'] =  sw['ports']
+        intsw['ansible_host'] = sw['ip']
+        idffullsw[sw['hostname']] = intsw
+    for sw in mdf:
+        intsw = {}
+        intsw['ports'] =  sw['ports']
+        intsw['ansible_host'] = sw['ip']
+        mdffullsw[sw['hostname']] = intsw
+        #devlist.append("{} ansiblehost= {} ports= {} hostname= {}" .format(sw['hostname'],sw['ip'],sw['ports'],sw['hostname']))
+    idfs['hosts'] = idffullsw
+    #print (json.dumps(mdffullsw,indent=2))
+    mdfs['hosts'] = mdffullsw
+    base['mdf'] = mdfs
+    #print (base['mdf'])
+    base['idf'] = idfs
+    iterat1['children'] = base
+    completedict['all'] = iterat1
+    #completedict['all'] = base
+    #print (yaml.dump(completedict))
+    return (json.dumps(completedict,indent=2))
+ 
+# Main Logging Start ####
+logger = logging.getLogger('SwInfo')
+# Main logging End ####
+
+# Arg Parse Main start
+# Parse Command Line Arguments
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+        description='Basic Discription: This script will use Aruba Central APIs to generate yaml invintory with hostname, ports, and mgmt ip for THD stores')
+#group set up for further development
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-g","--groupfile", help="Input file", type=str)
+parser.add_argument("-f","--file", help="filename to output json data *script will attach json extention", type=str)
+parser.add_argument("-v", help="Verbose Output", action="store_true")
+args = parser.parse_args()
+
+# Config Parse Start
+#Pull variables from config
+CONFIG_FILE = 'config.ini'
+# Initialize Config file and get the default log_level
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+log_level = int(config['main']['log_level'])
+
+host = str(config['main']['host'])
+token = str(config['main']['token'])
+outfile = args.file
+
+
+if args.v:
+    log_level = logging.INFO
+
+#Init logging
+init_logging()
+
+
+if args.groupfile:
+    mdfsw = []
+    idfsw = []
+    filename = args.groupfile
+    with open(filename,'r') as f:
+        groups = f.readlines()
+    for group in groups:
+        print('\n\n\n********************************')
+        print('[+] Starting API calls for group {} '.format(group))
+        grp = group.strip()
+        srchsrting = grp
+        tggrp = get_group(host,srchsrting,token)
+        print('[+] Group search returned  {} '.format(tggrp['group']))
+        print('[+] Moving to Switch inv API')
+        switches = get_switches(host,tggrp['group'],token)
+        #print (switches)
+        swinfo = swData(switches)
+        #print (swinfo)
+        print('[+] Getting switch data')
+        for x in swinfo:
+            ports = ""
+            if "swm" in x:
+                print('[+] Found Store MDF')
+                #print (swinfo[x])
+                ports = get_mdfports(host,swinfo[x]['switches'],token)
+                swinfo[x]['ports'] = ports
+                swinfo[x]['hostname'] = x
+                mdfsw.append(swinfo[x])
+                attip = get_4093ip(host,swinfo[x],token)
+                swinfo[x]['ip'] = attip
+            else:
+                ports = portconverter (swinfo[x]['switches'])
+                swinfo[x]['ports'] = ports
+                swinfo[x]['hostname'] = x
+                idfsw.append(swinfo[x])
+                #print(swinfo[x].keys())
+
+                attip = get_4093ip(host,swinfo[x],token)
+                swinfo[x]['ip'] = attip
+
+    if args.file:
+        output_file = args.file 
+    else:
+        output_file = 'inv.yml'
+    print('[+] Pushing Data for YAML file creation')
+    outputdata = ymlmaker(mdfsw,idfsw)
+    with open(output_file, 'w') as writefile:
+        writefile.write(outputdata)
+    print('[+] Invintory file created with filname -->>:  {}'.format(output_file))
+
+
