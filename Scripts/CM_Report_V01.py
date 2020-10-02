40e8110e09704c567e092a6d1e6eea07692086b0
mcjay18 <jaymcnealy@gmail.com>
2020-10-02 10:50:02 -0500

    add adjustment for down switch

--- Scripts/CM_Report_V01.py
+++ Scripts/CM_Report_V01.py
@@ -35,8 +35,12 @@ __status__ = "Testing"
 # pull raw data from ansible output
 def getraw(rawdata):
     # print (rawdata['stdout_lines'])
-
-    return((rawdata['stdout_lines']))
+    try:
+        return((rawdata['stdout_lines']))
+    except:
+        jsondata = json.loads(rawdata)
+        if jsondata['skipped'] == 'true':
+            return ("skipped")
 
 # create Dict from raw cable diagnostic data
 def cbldiag_parse(cbldata):
@@ -295,13 +299,15 @@ for idfs in (inv_dict['children']['idf']['hosts']):
 for mdfs in (inv_dict['children']['mdf']['hosts']):
     mdf_lst.append(mdfs)
 for idf in idf_lst:
-    #try:
-    filename = results_folder + idf + '.cable'
-    print (filename)
-    with open(filename, 'r') as idffile:
-        cableraw = getraw(json.loads(idffile.read()))
-    #except:
-    #    print ("filenotfound")
+    try:
+        filename = results_folder + idf + '.cable'
+        print (filename)
+        with open(filename, 'r') as idffile:
+            cableraw = getraw(json.loads(idffile.read()))
+        if cableraw == 'skipped':
+            break
+    except:
+        print ("filenotfound")
     try:
         filename = results_folder + idf + '.interface'
         with open(filename, 'r') as idffile:
