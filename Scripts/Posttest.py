6c417727cbcf250b8cbfcc3d7dcc09d0f22a7e23
mcjay18 <jaymcnealy@gmail.com>
2020-10-06 14:06:07 -0500

    add ping to tests

--- Scripts/Posttest.py
+++ Scripts/Posttest.py
@@ -19,3 +19,19 @@ def mdf_interface_parse(intdata):
             portdict['Drop_TX']= espl[4]
         sw_Int_Dict[espl[0]]=portdict
     return (sw_Int_Dict)
+
+results_folder = '../Results/'
+inv_folder = '../Inv/'
+
+# Arg Parse Main start
+# Parse Command Line Arguments
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+        description='Basic Discription: Script to generate CSV report from the output of Ansible CableDiag playbook')
+#group set up for further development
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-i","--invintory", help="Input file", type=str)
+parser.add_argument("-f","--file", help="report filename if differant then inv file", type=str)
+parser.add_argument("-v", help="Verbose Output", action="store_true")
+args = parser.parse_args()
+
